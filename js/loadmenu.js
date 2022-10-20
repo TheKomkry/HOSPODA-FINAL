4708ee1eac3bd1aed87484c07b45a5ce49be76cc
Komkry <Komkry@seznam.cz>
2022-10-20 12:12:37 +0200

    menu js & json

---
+++ js/loadmenu.js
@@ -0,0 +1,44 @@
+
+function loadMenu(){
+    fetch(`./data/MomentalniMenu.json`)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        writeMenu(data);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+function writeMenu(data){
+    loadDay(data.Pondeli, "pondeli");
+    loadDay(data.Utery, "utery");
+    loadDay(data.Streda, "streda");
+    loadDay(data.Ctvrtek, "ctvrtek");
+    loadDay(data.Patek, "patek");
+
+}
+function loadDay(data, day){
+    document.getElementById(day).innerHTML += `<small-date> - ${data.datum}</small-date>`;
+    const containerForDay = document.getElementById(`${day.substring(0,2)}-container`);
+    
+    let listJidel = data.jidla;
+    listJidel.forEach(element => {
+        // li > h2
+        const jidloListElem = document.createElement("li");
+        const jidloNazevElem = document.createElement("h2");
+        let jidloNazev = jidloListElem.appendChild(jidloNazevElem);
+        jidloNazev.innerHTML = element.nazev;
+        // li > p
+        let jidloPopisElem = document.createElement("p");
+        if (element.popis == undefined || element.popis == ""){
+            jidloPopisElem.innerHTML = `${element.cena} Kč`;
+        } else {
+            jidloPopisElem.innerHTML = `${element.popis} - ${element.cena} Kč`;
+        }
+        // create the elements
+        containerForDay.appendChild(jidloListElem);
+        containerForDay.appendChild(jidloPopisElem);
+    });
+}
