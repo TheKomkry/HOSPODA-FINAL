ad7929c935863e62b56f9e824a27d6dc41d95761
Komkry <Komkry@seznam.cz>
2023-04-30 22:57:49 +0200

    menu, zmeny na statni svatky

--- js/alertUser.js
+++ js/alertUser.js
@@ -1,6 +1,6 @@
-let alertText = "Kvůli svátkům je změna v otevírací době a v nabídce. Více informací na stránce Info & Akce.";
-let allowAlerts = false;
-let alertCode = 'ALERT_VANOCNI_2022';
+let alertText = "Pondělí 1. a 8. května ZAVŘENO, státní svátek.\n\nVíce informací na stránce Info & Akce. ";
+let allowAlerts = true;
+let alertCode = 'ALERT_KVETEN_STATNI-SVATEK_2023';
 function alertUser() {
     let hasBeenAlerted = localStorage['hasBeenAlerted'] || 'false';
     if (hasBeenAlerted != alertCode && allowAlerts) {
