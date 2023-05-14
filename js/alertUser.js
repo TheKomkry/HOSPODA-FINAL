d349f6fefc9d40e331999fd9d4dd4ad47d025700
Komkry <Komkry@seznam.cz>
2023-05-14 12:22:30 +0200

    menu 15.05.

--- js/alertUser.js
+++ js/alertUser.js
@@ -1,5 +1,5 @@
 let alertText = "Pondělí 1. a 8. května ZAVŘENO, státní svátek.\n\nVíce informací na stránce Info & Akce. ";
-let allowAlerts = true;
+let allowAlerts = false;
 let alertCode = 'ALERT_KVETEN_STATNI-SVATEK_2023';
 function alertUser() {
     let hasBeenAlerted = localStorage['hasBeenAlerted'] || 'false';
