ec1ddb955709e80941f67b534e4fed8ba5082542
Komkry <Komkry@seznam.cz>
2024-03-24 20:04:43 +0100

    velikonoce

--- js/alertUser.js
+++ js/alertUser.js
@@ -1,6 +1,6 @@
-let alertText = "Pondělí 1. a 8. května ZAVŘENO, státní svátek.\n\nVíce informací na stránce Info & Akce. ";
-let allowAlerts = false;
-let alertCode = 'ALERT_KVETEN_STATNI-SVATEK_2023';
+let alertText = "Pátek 29. 3. 2024 vaříme dle stálé nabídky.";
+let allowAlerts = true;
+let alertCode = 'ALERT_VALIKONOCE_2024';
 function alertUser() {
     let hasBeenAlerted = localStorage['hasBeenAlerted'] || 'false';
     if (hasBeenAlerted != alertCode && allowAlerts) {
