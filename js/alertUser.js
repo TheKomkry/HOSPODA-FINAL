0773e56e8ae120254ad8161070e38448b54c7069
Komkry <78811061+TheKomkry@users.noreply.github.com>
2024-04-28 11:52:26 +0200

    Update alertUser.js

--- js/alertUser.js
+++ js/alertUser.js
@@ -1,4 +1,4 @@
-let alertText = "OTEVÍRACÍ DOBA\n\nÚTERÝ     30.4.  11.00 - 14.00\nSTŘEDA     1.5.    18.00 - 22.00\n\nVíce informací na stránce Info & Akce. ";
+let alertText = "OTEVÍRACÍ DOBA\n\nÚTERÝ     30.4.  11.00 - 14.00\nSTŘEDA     1.5.    18.00 - 22.00\n\nVíce informací na stránce otevírací doba. ";
 let allowAlerts = true;
 let alertCode = 'ALERT_STATNI-SVATEK_24';
 function alertUser() {
