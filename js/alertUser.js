66044f78e76feb2ea328466b16214b18d1ebaf98
Komkry <Komkry@seznam.cz>
2024-08-04 14:32:26 +0200

    Nevarime 05.-11.8.

--- js/alertUser.js
+++ js/alertUser.js
@@ -1,6 +1,6 @@
-let alertText = "Pátek 29. 3. 2024 vaříme dle stálé nabídky.";
+let alertText = "Oznamujeme, že z provozních důvodů v týdnu od 5. do 11. srpna 2024 nebudeme vařit. \nOtevírací doba zůstává nezměněna a vše se vrací do normálu od pondělí 12. srpna 2024. \n\nDěkujeme za pochopení.";
 let allowAlerts = true;
-let alertCode = 'ALERT_VALIKONOCE_2024';
+let alertCode = 'NEVARIME_SRPEN_2024_05-11';
 function alertUser() {
     let hasBeenAlerted = localStorage['hasBeenAlerted'] || 'false';
     if (hasBeenAlerted != alertCode && allowAlerts) {
