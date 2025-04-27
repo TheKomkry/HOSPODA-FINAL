0a336558cdf7b3822007b8ccc8bcf644d6e0dfd7
Komkry <78811061+TheKomkry@users.noreply.github.com>
2025-04-27 08:38:01 +0200

    Update alertUser.js

--- js/alertUser.js
+++ js/alertUser.js
@@ -1,5 +1,5 @@
 let alertText = "Otevírací doba přes velikonočí svátky byla zveřejněna v Info & Akce nebo v Otevírací doba \n\nDěkujeme za pochopení.";
-let allowAlerts = true;
+let allowAlerts = false;
 let alertCode = 'VELIKONOCE2025';
 function alertUser() {
     let hasBeenAlerted = localStorage['hasBeenAlerted'] || 'false';
