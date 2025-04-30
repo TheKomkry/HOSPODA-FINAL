3c2cb82738b33325ceb922dc6780c79cbe8e458e
Komkry <Komkry@seznam.cz>
2025-04-30 22:29:18 +0200

    smazat úpravu otevírací doby

--- js/alertUser.js
+++ js/alertUser.js
@@ -1,5 +1,5 @@
 let alertText = "Otevírací doba tento týden byla zveřejněna v Info & Akce \n\nDěkujeme za pochopení.";
-let allowAlerts = true;
+let allowAlerts = false;
 let alertCode = 'STATNISVATEK-1.5.2025';
 function alertUser() {
     let hasBeenAlerted = localStorage['hasBeenAlerted'] || 'false';
