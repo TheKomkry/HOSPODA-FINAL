abd49181c87e8f1b27b1bb0aa8ce960c60d01076
Komkry <Komkry@seznam.cz>
2025-12-23 20:18:04 +0100

    oteviraci doba pres vanoce

--- js/alertUser.js
+++ js/alertUser.js
@@ -1,6 +1,6 @@
-let alertText = "Otevírací doba tento týden byla zveřejněna v Info & Akce \n\nDěkujeme za pochopení.";
-let allowAlerts = false;
-let alertCode = 'STATNISVATEK-1.5.2025';
+let alertText = "Otevírací doba tyto vánoce byla zveřejněna v Info & Akce \n\nDěkujeme za pochopení.";
+let allowAlerts = true;
+let alertCode = 'VANOCE25';
 function alertUser() {
     let hasBeenAlerted = localStorage['hasBeenAlerted'] || 'false';
     if (hasBeenAlerted != alertCode && allowAlerts) {
